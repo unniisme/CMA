5559d7a27bf138a7124f9b60e5462f8abda63337
unniisme <122001019@smail.iitpkd.ac.in>
2023-04-19 12:47:39 +0530

    Remade

--- Fluid Simulation/NavierStokes.py
+++ Fluid Simulation/NavierStokes.py
@@ -1,220 +1,172 @@
-## References : 
-# https://www.autodesk.com/research/publications/real-time-fluid-dynamics
-# https://en.wikipedia.org/wiki/Navier%E2%80%93Stokes_equations
-# https://hplgit.github.io/INF5620/doc/pub/main_ns.pdf
-
 import numpy as np
 
-lerp = lambda a,b,k: a + k*(b-a)
 
+class FluidSolver:
+    def __init__(self, N, diff, visc, dt):
+        self.N = N
+        self.dt = dt
+        self.diff = diff
+        self.visc = visc
 
-class NS_sim:
+        # allocate memory for velocity and density fields
+        size = (N+2)*(N+2)
+        self.size = size
+        self.u = [0.0]*size
+        self.v = [0.0]*size
+        self.u_prev = [0.0]*size
+        self.v_prev = [0.0]*size
+        self.density = [0.0]*size
+        self.density_prev = [0.0]*size
 
-    dt = 0.01
-    diff_coeff = 0.1
-    visc_coeff = 1
+    def add_source(self, s):
+        for i in range((self.N+2)*(self.N+2)):
+            self.density[i] += self.dt*s[i]
 
-    iterations = 20
+    def set_bnd(self, b, x):
+        for i in range(1, self.N+1):
+            x[self.IX(0,i)] = -x[self.IX(1,i)] if b == 1 else x[self.IX(1,i)]
+            x[self.IX(self.N+1,i)] = -x[self.IX(self.N,i)] if b == 1 else x[self.IX(self.N,i)]
+            x[self.IX(i,0)] = -x[self.IX(i,1)] if b == 2 else x[self.IX(i,1)]
+            x[self.IX(i,self.N+1)] = -x[self.IX(i,self.N)] if b == 2 else x[self.IX(i,self.N)]
 
-    def __init__(self, dimensions : tuple):
-        self.dimensions = dimensions
-        self.D = np.zeros(dimensions)
-        self.V = np.zeros((dimensions[0], dimensions[1], 2))
-        self.D_prev = np.zeros(dimensions)
-        self.V_prev = np.zeros((dimensions[0], dimensions[1], 2))
+        x[self.IX(0,0)] = 0.5*(x[self.IX(1,0)]+x[self.IX(0,1)])
+        x[self.IX(0,self.N+1)] = 0.5*(x[self.IX(1,self.N+1)]+x[self.IX(0,self.N)])
+        x[self.IX(self.N+1,0)] = 0.5*(x[self.IX(self.N,0)]+x[self.IX(self.N+1,1)])
+        x[self.IX(self.N+1,self.N+1)] = 0.5*(x[self.IX(self.N,self.N+1)]+x[self.IX(self.N+1,self.N)])
 
-        self._params = {"D" : [self.D, self.D_prev], "V" : [self.V, self.V_prev]}
+    def IX(self, i, j):
+        return i + (self.N+2)*j
 
-    def diffuse(self, param, const, dt):
+    def lin_solve(self, b, x, x0, a, c):
+        for k in range(20):
+            for i in range(1, self.N+1):
+                for j in range(1, self.N+1):
+                    x[self.IX(i,j)] = (x0[self.IX(i,j)] + a*(x[self.IX(i-1,j)]+x[self.IX(i+1,j)]+x[self.IX(i,j-1)]+x[self.IX(i,j+1)]))/c
+            self.set_bnd(b, x)
 
-        k = const*dt*self.dimensions[0]*self.dimensions[1]
+    def diffuse(self, x, x0, diff):
+        a = self.dt*diff*self.N*self.N
+        self.lin_solve(0, x, x0, a, 1+4*a)
 
-        old_param = self._params[param][0].copy()
+    def advect(self, d, d0, u, v):
+        dt0 = self.dt*self.N
+        for i in range(1, self.N+1):
+            for j in range(1, self.N+1):
+                x = i-dt0*u[self.IX(i,j)]
+                y = j-dt0*v[self.IX(i,j)]
+                if x < 0.5:
+                    x = 0.5
+                if x > self.N+0.5:
+                    x = self.N+0.5
+                i0 = int(x)
+                i1 = i0+1
+                if y < 0.5:
+                    y = 0.5
+                if y > self.N+0.5:
+                    y = self.N+0.5
+                j0 = int(y)
+                j1 = j0+1
+                s1 = x-i0
+                s0 = 1-s1
+                t1 = y-j0
+                t0 = 1-t1
+                d[self.IX(i,j)] = s0*(t0*d0[self.IX(i0,j0)]+t1*d0[self.IX(i0,j1)])+s1*(t0*d0[self.IX(i1,j0)]+t1*d0[self.IX(i1,j1)])
+        self.set_bnd(0, d)
 
-        for interation in range(self.iterations):
-            for i in range(self.dimensions[0]):
-                for j in range(self.dimensions[1]):
-                    try:
-                        s = (self._params[param][0][i+1,j] + self._params[param][0][i-1,j] + self._params[param][0][i,j+1] + self._params[param][0][i,j-1])
-                        self._params[param][0][i,j] = (4*self._params[param][1][i,j] + k * s)/(4+4*k)
-                    except:
-                        pass
+    def project(self):
+        for i in range(1, self.N+1):
+            for j in range(1, self.N+1):
+                self.u_prev[self.IX(i,j)] = self.u[self.IX(i,j)]
+                self.v_prev[self.IX(i,j)] = self.v[self.IX(i,j)]
+                self.density_prev[self.IX(i,j)] = self.density[self.IX(i,j)]
 
-        self._params[param][1] = old_param
+        self.divergence()
+        self.pressure = [0.0]*self.size
 
-    def advect(self, param, dt, iterations = 20):
+        self.lin_solve(0, self.pressure, self.div, 1, 4)
 
-        old_param = self._params[param][0].copy()
+        for i in range(1, self.N+1):
+            for j in range(1, self.N+1):
+                self.u[self.IX(i,j)] -= 0.5*self.N*(self.pressure[self.IX(i+1,j)]-self.pressure[self.IX(i-1,j)])
+                self.v[self.IX(i,j)] -= 0.5*self.N*(self.pressure[self.IX(i,j+1)]-self.pressure[self.IX(i,j-1)])
 
-        for i in range(self.dimensions[0]):
-            for j in range(self.dimensions[1]):
-                f = np.array([i,j]) - dt*self.V[i,j]
-                f = np.clip(f, [0.5,0.5], [self.dimensions[0]-1.5, self.dimensions[1]-1.5])
+        self.set_bnd(1, self.u)
+        self.set_bnd(2, self.v)
 
-                fract, floor = np.modf(f)
-                floor = np.asarray(floor, int)
+    def divergence(self):
+        self.div = [0.0]*self.size
+        for i in range(1, self.N+1):
+            for j in range(1, self.N+1):
+                self.div[self.IX(i,j)] = -0.5*(self.u[self.IX(i+1,j)]-self.u[self.IX(i-1,j)]+self.v[self.IX(i,j+1)]-self.v[self.IX(i,j-1)])/self.N
 
-                z1 = lerp(self._params[param][0][floor[0], floor[1]], self._params[param][0][floor[0]+1, floor[1]], fract[0])
-                z2 = lerp(self._params[param][0][floor[0], floor[1]+1], self._params[param][0][floor[0]+1, floor[1]+1], fract[0])
-                self._params[param][0][i,j] = lerp(z1, z2, fract[1])
-                
-                # x = i - dt*self.V[i,j][0]
-                # y = j - dt*self.V[i,j][1]
+        self.set_bnd(0, self.div)
 
-                # x = max(0.5, min(self.dimensions[0]-1.5, x))
-                # y = max(0.5, min(self.dimensions[1]-1.5, y))
+    def vel_step(self):
+        self.add_source(self.u_prev)
+        self.add_source(self.v_prev)
 
-                # i0 = int(x)
-                # i1 = i0+1
-                # j0 = int(y)
-                # j1 = j0+1
+        self.diffuse(self.u, self.u_prev, self.visc)
+        self.diffuse(self.v, self.v_prev, self.visc)
 
-                # s1 = x-i1
-                # s0 = 1-s1
-                # t1 = y-j1
-                # t0 = 1-t1
+        self.project()
 
-                # self._params[param][0][i,j] = s0*(t0*self._params[param][1][i0,j0] + t1*self._params[param][1][i0,j1]) + s1*(t0*self._params[param][1][i1,j0] + t1*self._params[param][1][i1,j1])
+        for i in range(1, self.N+1):
+            for j in range(1, self.N+1):
+                self.u_prev[self.IX(i,j)] = self.u[self.IX(i,j)]
+                self.v_prev[self.IX(i,j)] = self.v[self.IX(i,j)]
 
-        self._params[param][1] = old_param
+        self.advect(self.u, self.u_prev, self.u_prev, self.v_prev)
+        self.advect(self.v, self.v_prev, self.u_prev, self.v_prev)
 
+        self.project()
 
-    def curl(self):
+    def dens_step(self):
+        self.add_source(self.density_prev)
 
-        div = np.zeros(self.dimensions)
-        p = np.zeros(self.dimensions)
-        for i in range(1,self.dimensions[0]-1):
-            for j in range(1, self.dimensions[1]-1):
-                div[i,j] = (self.V[i+1, j][0] - self.V[i-1, j][0] + self.V[i, j+1][1] - self.V[i, j-1][1])*0.5
+        self.diffuse(self.density, self.density_prev, self.diff)
+        self.advect(self.density_prev, self.density, self.u, self.v)
 
-        
-        for iteration in range(NS.iterations):
-            for i in range(1,self.dimensions[0]-1):
-                for j in range(1, self.dimensions[1]-1):    
-                    p[i,j] = (p[i-1, j] + p[i+1,j] + p[i, j-1] + p[i, j+1]  -  div[i,j])/4
+class SmokeSimulation:
+    def __init__(self, N, diff, visc, dt):
+        self.solver = FluidSolver(N, diff, visc, dt)
 
-        k = 0.5*self.dimensions[0] #Keeping it like this for now, in reference to paper
-        for i in range(1,self.dimensions[0]-1):
-            for j in range(1, self.dimensions[1]-1):
-                self.V[i,j][0] -= k*p[i+1,j] - p[i-1, j]
-                self.V[i,j][1] -= k*p[i,j+1] - p[i, j+1]
+    def add_density(self, x, y, amount):
+        size = 1
+        self.solver.density[self.solver.IX(int(x*size), int(y*size))] += amount
 
+    def add_velocity(self, x, y, amountX, amountY):
+        size = 1
+        self.solver.u[self.solver.IX(int(x*size), int(y*size))] += amountX
+        self.solver.v[self.solver.IX(int(x*size), int(y*size))] += amountY
 
-    def update(self, dt):
+    def update(self):
+        self.solver.vel_step()
+        self.solver.dens_step()
 
-        # Upadate velocity
-        self.diffuse("V", self.visc_coeff, dt)
-        self.curl()
-        self.advect("V", dt)
-        self.curl()
+    def get_density(self):
+        N = self.solver.N
+        density = np.zeros((N, N))
+        for i in range(N):
+            for j in range(N):
+                density[i,j] = self.solver.density[self.solver.IX(i,j)]
+        return density
 
+# To use this code, you can create a `SmokeSimulation` object with the desired parameters (grid size, diffusion, viscosity, time step). Then, you can call the `add_density` and `add_velocity` methods to add sources of smoke and wind to the simulation. Finally, you can call the `update` method to advance the simulation by one time step. Here's an example:
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
 
-        # Update density
-        # self.D_prev, self.D = self.D, self.D_prev
-        self.diffuse("D", self.diff_coeff, dt)
-        # self.D_prev, self.D = self.D, self.D_prev
-        self.advect("D", dt)
+    # create smoke simulation with 64x64 grid, diffusion=0.01, viscosity=0.1, dt=0.1
+    sim = SmokeSimulation(64, 0.01, 0.1, 0.1)
 
+    # add some smoke and wind sources
+    sim.add_density(0.5, 0.5, 100)
+    sim.add_velocity(0.5, 0.5, 2, 0)
 
-class NS:
+    # run simulation for 100 time steps
+    for i in range(100):
+        print (i)
+        sim.update()
 
-    dt = 0.01
-    diff_coeff = 10
-    visc_coeff = 1
-
-    iterations = 20 #Number of iterations for guass-siedel method
-
-    def __init__(self, dimensions : tuple):
-        self.dimensions = dimensions
-        self.D = np.zeros(dimensions)
-        self.V = np.zeros((dimensions[0], dimensions[1], 2))
-        self.D_prev = np.zeros(dimensions)
-        self.V_prev = np.zeros((dimensions[0], dimensions[1], 2))
-
-    def diffuse(self, x, x0, const, dt):
-        k = const*dt*self.dimensions[0]*self.dimensions[1]
-
-        old_param = x.copy() #Handle outside
-
-        for interation in range(NS.iterations):
-            for i in range(1,self.dimensions[0]-1):
-                for j in range(1,self.dimensions[1]-1):
-                    s = (x[i+1,j] + x[i-1,j] + x[i,j+1] + x[i,j-1])
-                    x[i,j] = (4*x0[i,j] + k * s)/(4+4*k)
-
-        x0 = old_param  #Handle outside
-
-    def advect(self, x, x0, v, dt):
-        dt = dt*(np.sqrt(self.dimensions[0]*self.dimensions[1]))
-
-        # old_x = x.copy() #Handle outside
-
-        for i in range(self.dimensions[0]):
-            for j in range(self.dimensions[1]):
-                f = np.array([i,j]) - dt*v[i,j]
-                f = np.clip(f, [0.5,0.5], [self.dimensions[0]-1.5, self.dimensions[1]-1.5])
-
-                fract, floor = np.modf(f)
-                floor = np.asarray(floor, int)
-
-                z1 = lerp(x[floor[0], floor[1]], x[floor[0]+1, floor[1]], fract[0])
-                z2 = lerp(x[floor[0], floor[1]+1], x[floor[0]+1, floor[1]+1], fract[0])
-                x[i,j] = lerp(z1, z2, fract[1])
-                
-                # x = i - dt*v[i,j][0]
-                # y = j - dt*v[i,j][1]
-
-                # x = max(0.5, min(self.dimensions[0]-1.5, x))
-                # y = max(0.5, min(self.dimensions[1]-1.5, y))
-
-                # i0 = int(x)
-                # i1 = i0+1
-                # j0 = int(y)
-                # j1 = j0+1
-
-                # s1 = x-i1
-                # s0 = 1-s1
-                # t1 = y-j1
-                # t0 = 1-t1
-
-                # self.D[i,j] = s0*(t0*x0[i0,j0] + t1*x0[i0,j1]) + s1*(t0*x0[i1,j0] + t1*x0[i1,j1])
-
-        # x0 = old_x #Handle externally
-
-    def curl(self, v):
-
-        div = np.zeros(self.dimensions)
-        p = np.zeros(self.dimensions)
-        for i in range(1,self.dimensions[0]-1):
-            for j in range(1, self.dimensions[1]-1):
-                div[i,j] = (v[i+1, j][0] - v[i-1, j][0] + v[i, j+1][1] - v[i, j-1][1])*0.5
-
-        
-        for iteration in range(NS.iterations):
-            for i in range(1,self.dimensions[0]-1):
-                for j in range(1, self.dimensions[1]-1):    
-                    p[i,j] = (p[i-1, j] + p[i+1,j] + p[i, j-1] + p[i, j+1]  -  div[i,j])/4
-
-        k = 0.5*self.dimensions[0] #Keeping it like this for now, in reference to paper
-        for i in range(1,self.dimensions[0]-1):
-            for j in range(1, self.dimensions[1]-1):
-                v[i,j][0] -= k*p[i+1,j] - p[i-1, j]
-                v[i,j][1] -= k*p[i,j+1] - p[i, j+1]
-
-
-    def update(self, dt):
-
-        # Upadate velocity
-        self.V_prev, self.V = self.V, self.V_prev
-        self.diffuse(self.V, self.V_prev, self.visc_coeff, dt)
-        self.curl(self.V)
-        self.V_prev, self.V = self.V, self.V_prev
-        self.advect(self.V, self.V_prev, self.V, dt)
-        self.curl(self.V)
-
-
-        # Update density
-        # self.D_prev, self.D = self.D, self.D_prev
-        self.diffuse(self.D, self.D_prev, self.diff_coeff, dt)
-        # self.D_prev, self.D = self.D, self.D_prev
-        self.advect(self.D, self.D_prev, self.V, dt)
+    # display density field as grayscale image
+    plt.imshow(np.array(sim.solver.density).reshape(sim.solver.N+2, sim.solver.N+2), cmap='gray')
+    plt.show()
--- Fluid Simulation/simulator.py
+++ Fluid Simulation/simulator.py
@@ -1,20 +1,22 @@
 import pygame
-from NavierStokes import NS, NS_sim
-import numpy as np
+from NavierStokes import SmokeSimulation
 
 class Simulator:
 
-    def __init__ (self, screenSize = [800, 600], gridSize = 10):
+    def __init__ (self, N = 20, resolution = 30):
         self.fps = 60
+        screenSize = [N*resolution]*2
         self.screen = pygame.display.set_mode(screenSize)
-        self.screenSize = screenSize
-        self.gridSize = gridSize
         self.clock = pygame.time.Clock()
         self.running = True
 
-        self.dimensions = (screenSize[0]//gridSize, screenSize[1]//gridSize)
+        self.force = 500
+        self.source = 1000
 
-        self.fluid = NS_sim(self.dimensions)
+        self.gridSize = resolution
+        self.dimensions = (N, N)
+
+        self.fluid = SmokeSimulation(N, 0.01, 0.01, 0.1)
 
     def DrawSquare(self, gridStart : tuple, length : int, offset : int, colour : tuple = (0,0,255)):
         rect = pygame.Rect(gridStart[0] + offset, gridStart[1] + offset, length - 2*offset, length - 2*offset)
@@ -24,7 +26,7 @@ class Simulator:
 
         for i in range(self.dimensions[0]):
             for j in range(self.dimensions[1]):
-                col = round(255 - self.fluid.D[i,j]*255/500)
+                col = round(255 - self.fluid.get_density()[i,j]*255/500)
                 col = max(0, min(255,col))
                 self.DrawSquare((i*self.gridSize,j*self.gridSize), self.gridSize, in_offset, (col,col,col))
     
@@ -40,26 +42,22 @@ class Simulator:
     def Start(self):
 
         while True:
+
             for e in pygame.event.get():
                 if e.type == pygame.QUIT:
                     return 0
 
             mouse_pos = pygame.mouse.get_pos()
+            mouse_delta = pygame.mouse.get_rel()
 
+            if pygame.mouse.get_pressed()[0]:
+                self.fluid.add_density(*self.Vector2ToGridCell(mouse_pos), self.source)
+
+            self.fluid.add_velocity(*self.Vector2ToGridCell(mouse_pos), *self.Vector2ToGridCell(mouse_delta*self.force))
+
+            self.fluid.update()
 
             self.screen.fill((255,255,255))
-
-            self.fluid.D[self.Vector2ToGridCell(mouse_pos)] += (1/self.fps) * 100000
-            self.fluid.D_prev[self.Vector2ToGridCell(mouse_pos)] += (1/self.fps) * 100000
-
-            # for i in range(self.fluid.dimensions[0]):
-            #     for j in range(self.fluid.dimensions[1]):
-            #         pygame.draw.line(self.screen, (0,0,0), (se), end_pos)
-
-            self.fluid.update(1/self.fps)
-            # self.fluid.diffuse(self.fluid.D, self.fluid.D_prev, self.fluid.diff_coeff, 1/self.fps)
-
-            print(np.argmax(self.fluid.D), np.max(self.fluid.D))
 
             self.DrawGrid()
 
@@ -68,7 +66,7 @@ class Simulator:
 
 
 if __name__ == '__main__':
-    sim = Simulator(gridSize = 50)
+    sim = Simulator()
 
     # sim.fluid.D[5,5] = 1000
     # sim.fluid.D_prev[5,5] = 1000
