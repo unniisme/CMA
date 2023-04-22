66c7d6cf95cd59c56eda5dc5c1bf5b6a450d2117
unniisme <122001019@smail.iitpkd.ac.in>
2023-04-22 20:59:16 +0530

    Updated to np arrays Super fast now!!

--- Fluid Simulation/NavierStokes.py
+++ Fluid Simulation/NavierStokes.py
@@ -9,40 +9,36 @@ class FluidSolver:
         self.visc = visc
 
         # allocate memory for velocity and density fields
+        self.dimensions = (N+2, N+2)
         size = (N+2)*(N+2)
         self.size = size
-        self.u = [0.0]*size
-        self.v = [0.0]*size
-        self.u_prev = [0.0]*size
-        self.v_prev = [0.0]*size
-        self.density = [0.0]*size
-        self.density_prev = [0.0]*size
+        self.u = np.zeros(self.dimensions)
+        self.v = np.zeros(self.dimensions)
+        self.u_prev = np.zeros(self.dimensions)
+        self.v_prev = np.zeros(self.dimensions)
+        self.density = np.zeros(self.dimensions)
+        self.density_prev = np.zeros(self.dimensions)
 
     def add_source(self, s):
-        for i in range((self.N+2)*(self.N+2)):
-            self.density[i] += self.dt*s[i]
+        self.density += self.dt*s
 
     def set_bnd(self, b, x):
-        for i in range(1, self.N+1):
-            x[self.IX(0,i)] = -x[self.IX(1,i)] if b == 1 else x[self.IX(1,i)]
-            x[self.IX(self.N+1,i)] = -x[self.IX(self.N,i)] if b == 1 else x[self.IX(self.N,i)]
-            x[self.IX(i,0)] = -x[self.IX(i,1)] if b == 2 else x[self.IX(i,1)]
-            x[self.IX(i,self.N+1)] = -x[self.IX(i,self.N)] if b == 2 else x[self.IX(i,self.N)]
+        x[0,:] = np.where(b==1, -x[1,:], x[1,:])
+        x[self.N+1,:] = np.where(b==1, -x[self.N,:], x[self.N,:])
+        x[:,0] = np.where(b==2, -x[:,1], x[:,1])
+        x[:,self.N+1] = np.where(b==2, -x[:,self.N], x[:,self.N])
 
-        x[self.IX(0,0)] = 0.5*(x[self.IX(1,0)]+x[self.IX(0,1)])
-        x[self.IX(0,self.N+1)] = 0.5*(x[self.IX(1,self.N+1)]+x[self.IX(0,self.N)])
-        x[self.IX(self.N+1,0)] = 0.5*(x[self.IX(self.N,0)]+x[self.IX(self.N+1,1)])
-        x[self.IX(self.N+1,self.N+1)] = 0.5*(x[self.IX(self.N,self.N+1)]+x[self.IX(self.N+1,self.N)])
+        x[0,0] = 0.5*(x[1,0]+x[0,1])
+        x[0,self.N+1] = 0.5*(x[1,self.N+1]+x[0,self.N])
+        x[self.N+1,0] = 0.5*(x[self.N,0]+x[self.N+1,1])
+        x[self.N+1,self.N+1] = 0.5*(x[self.N,self.N+1]+x[self.N+1,self.N])
 
-    def IX(self, i, j):
-        return i + (self.N+2)*j
 
     def lin_solve(self, b, x, x0, a, c):
         for k in range(20):
-            for i in range(1, self.N+1):
-                for j in range(1, self.N+1):
-                    x[self.IX(i,j)] = (x0[self.IX(i,j)] + a*(x[self.IX(i-1,j)]+x[self.IX(i+1,j)]+x[self.IX(i,j-1)]+x[self.IX(i,j+1)]))/c
+            x[1:-1, 1:-1] = (x0[1:-1, 1:-1] + a*(x[:-2, 1:-1] + x[2:, 1:-1] + x[1:-1, :-2] + x[1:-1, 2:])) / c
             self.set_bnd(b, x)
+
 
     def diffuse(self, x, x0, diff):
         a = self.dt*diff*self.N*self.N
@@ -52,8 +48,8 @@ class FluidSolver:
         dt0 = self.dt*self.N
         for i in range(1, self.N+1):
             for j in range(1, self.N+1):
-                x = i-dt0*u[self.IX(i,j)]
-                y = j-dt0*v[self.IX(i,j)]
+                x = i-dt0*u[(i,j)]
+                y = j-dt0*v[(i,j)]
                 if x < 0.5:
                     x = 0.5
                 if x > self.N+0.5:
@@ -70,34 +66,35 @@ class FluidSolver:
                 s0 = 1-s1
                 t1 = y-j0
                 t0 = 1-t1
-                d[self.IX(i,j)] = s0*(t0*d0[self.IX(i0,j0)]+t1*d0[self.IX(i0,j1)])+s1*(t0*d0[self.IX(i1,j0)]+t1*d0[self.IX(i1,j1)])
+                d[(i,j)] = s0*(t0*d0[(i0,j0)]+t1*d0[(i0,j1)])+s1*(t0*d0[(i1,j0)]+t1*d0[(i1,j1)])
         self.set_bnd(0, d)
+
 
     def project(self):
         for i in range(1, self.N+1):
             for j in range(1, self.N+1):
-                self.u_prev[self.IX(i,j)] = self.u[self.IX(i,j)]
-                self.v_prev[self.IX(i,j)] = self.v[self.IX(i,j)]
-                self.density_prev[self.IX(i,j)] = self.density[self.IX(i,j)]
+                self.u_prev[(i,j)] = self.u[(i,j)]
+                self.v_prev[(i,j)] = self.v[(i,j)]
+                self.density_prev[(i,j)] = self.density[(i,j)]
 
         self.divergence()
-        self.pressure = [0.0]*self.size
+        self.pressure = np.zeros(self.dimensions)
 
         self.lin_solve(0, self.pressure, self.div, 1, 4)
 
         for i in range(1, self.N+1):
             for j in range(1, self.N+1):
-                self.u[self.IX(i,j)] -= 0.5*self.N*(self.pressure[self.IX(i+1,j)]-self.pressure[self.IX(i-1,j)])
-                self.v[self.IX(i,j)] -= 0.5*self.N*(self.pressure[self.IX(i,j+1)]-self.pressure[self.IX(i,j-1)])
+                self.u[(i,j)] -= 0.5*self.N*(self.pressure[(i+1,j)]-self.pressure[(i-1,j)])
+                self.v[(i,j)] -= 0.5*self.N*(self.pressure[(i,j+1)]-self.pressure[(i,j-1)])
 
         self.set_bnd(1, self.u)
         self.set_bnd(2, self.v)
 
     def divergence(self):
-        self.div = [0.0]*self.size
+        self.div = np.zeros(self.dimensions)
         for i in range(1, self.N+1):
             for j in range(1, self.N+1):
-                self.div[self.IX(i,j)] = -0.5*(self.u[self.IX(i+1,j)]-self.u[self.IX(i-1,j)]+self.v[self.IX(i,j+1)]-self.v[self.IX(i,j-1)])/self.N
+                self.div[(i,j)] = -0.5*(self.u[(i+1,j)]-self.u[(i-1,j)]+self.v[(i,j+1)]-self.v[(i,j-1)])/self.N
 
         self.set_bnd(0, self.div)
 
@@ -112,8 +109,8 @@ class FluidSolver:
 
         for i in range(1, self.N+1):
             for j in range(1, self.N+1):
-                self.u_prev[self.IX(i,j)] = self.u[self.IX(i,j)]
-                self.v_prev[self.IX(i,j)] = self.v[self.IX(i,j)]
+                self.u_prev[(i,j)] = self.u[(i,j)]
+                self.v_prev[(i,j)] = self.v[(i,j)]
 
         self.advect(self.u, self.u_prev, self.u_prev, self.v_prev)
         self.advect(self.v, self.v_prev, self.u_prev, self.v_prev)
@@ -131,25 +128,18 @@ class SmokeSimulation:
         self.solver = FluidSolver(N, diff, visc, dt)
 
     def add_density(self, x, y, amount):
-        size = 1
-        self.solver.density[self.solver.IX(int(x*size), int(y*size))] += amount
+        self.solver.density[(int(x), int(y))] += amount
 
     def add_velocity(self, x, y, amountX, amountY):
-        size = 1
-        self.solver.u[self.solver.IX(int(x*size), int(y*size))] += amountX
-        self.solver.v[self.solver.IX(int(x*size), int(y*size))] += amountY
+        self.solver.u[(int(x), int(y))] += amountX
+        self.solver.v[(int(x), int(y))] += amountY
 
     def update(self):
         self.solver.vel_step()
         self.solver.dens_step()
 
     def get_density(self):
-        N = self.solver.N
-        density = np.zeros((N, N))
-        for i in range(N):
-            for j in range(N):
-                density[i,j] = self.solver.density[self.solver.IX(i,j)]
-        return density
+        return self.solver.density
 
 # To use this code, you can create a `SmokeSimulation` object with the desired parameters (grid size, diffusion, viscosity, time step). Then, you can call the `add_density` and `add_velocity` methods to add sources of smoke and wind to the simulation. Finally, you can call the `update` method to advance the simulation by one time step. Here's an example:
 if __name__ == '__main__':
